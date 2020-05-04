8d5d00db92955e2fee5e72c4234c9a462ecc04f9
ziredward <63553672+ziredward@users.noreply.github.com>
2020-05-03 23:42:01 -0500

    Update and rename src/app/vehicles/vehicles.component.spec.ts to  src/ aplicación/ vehículos/vehicles.component.spec.ts

---
+++ src/ aplicación/ vehículos/vehicles.component.spec.ts
@@ -0,0 +1,25 @@
+importar  {  async ,  ComponentFixture ,  TestBed  }  desde  '@ angular / core / testing' ;
+
+importar  {  VehiclesComponent  }  desde  './vehicles.component' ;
+
+describe ( 'VehiclesComponent' ,  ( )  =>  {
+   componente let : VehiclesComponent ;
+  let  fixture : ComponentFixture < VehiclesComponent > ;
+
+  beforeEach ( async ( ( )  =>  {
+    TestBed . configureTestingModule ( {
+      declaraciones : [  VehiclesComponent  ]
+    } )
+    . compileComponents ( ) ;
+  } ) ) ;
+
+  beforeEach ( ( )  =>  {
+    accesorio  =  TestBed . createComponent ( VehiclesComponent ) ;
+    componente  =  accesorio . componenteInstance ;
+    accesorio . detectChanges ( ) ;
+  } ) ;
+
+  it ( 'should create' ,  ( )  =>  {
+    esperar ( componente ) . toBeTruthy ( ) ;
+  } ) ;
+} ) ;
