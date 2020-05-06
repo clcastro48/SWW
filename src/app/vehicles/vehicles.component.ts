a3ae3dcee29bf1d75b160b877c6d9143ea5df44c
ziredward <63553672+ziredward@users.noreply.github.com>
2020-05-05 21:31:51 -0500

    Update vehicles.component.ts

--- src/app/vehicles/vehicles.component.ts
+++ src/app/vehicles/vehicles.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { NavesService } from '../naves.service';
+
+
 
 @Component({
   selector: 'app-vehicles',
-  templateUrl: './vehicles.component.html',
+  templateUrl:'./vehicles.component.html',
   styleUrls: ['./vehicles.component.css']
 })
 export class VehiclesComponent implements OnInit {
 
-  constructor() { }
+  
+  listadepeliculas: [];
+
+ constructor (private navesservice: NavesService) {
+
+  this.listadepeliculas = [];
+
+
+  
+  this.llenarlistadepeliculas ();
+ }
+
+ llenarlistadepeliculas () {
+
+  this.navesservice.traertodaslaspeliculas ().subscribe (
+    (data) => {
+      console.log ("DATA: " ,data);
+      this.listadepeliculas = data [ "results" ];
+
+    }
+  );
+  
+ }
+
 
   ngOnInit(): void {
   }
 
-}
+} 
